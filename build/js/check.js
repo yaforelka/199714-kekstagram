739718b6eed7d471a9b932acfd159be21307df0d
Maria-Izvekova <paravoznica@mail.ru>
2016-05-26 20:08:39 +0300

    Модуль 1 задание 2
    Добавлены 2 скрипта в index.html.  В check.js написана функция для
    вывода статистических сообщений.

---
+++ build/js/check.js
@@ -0,0 +1,25 @@
+function getMessage(a, b) {
+   if (typeof a == 'boolean') {
+    if (a) {
+	  return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+	}  else {
+	  return 'Переданное GIF-изображение не анимировано';
+	}
+  }	else if (typeof a == 'number') {
+	  return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' аттрибутов';
+    } else if (typeof a == 'object') {
+		if (typeof b == 'object') {
+			var square = 0;
+			for (var i = 0; i < a.length; i++){
+				square += a[i]*b[i];
+			};
+			return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
+		} else {
+			var sum = 0;
+		for (var i = 0; i < a.length; i++) {
+			sum += a[i];
+		};
+	    return 'Количество красных точек во всех строчках изображения: ' + sum;
+	    }
+	}
+};
