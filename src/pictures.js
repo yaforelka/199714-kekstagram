7e3a67fa8fc88ff24a286ccbc321f6ede6b8ff23
yaforelka <paravoznica@mail.ru>
2016-06-09 12:52:24 +0300

    Добавлены обработчики событий при загрузке изображений: onload и onerror.

--- src/pictures.js
+++ src/pictures.js
@@ -15,14 +15,19 @@ var getPictureElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
   element.querySelector('.picture-comments').textContent = data.comments;
   element.querySelector('.picture-likes').textContent = data.likes;
+  var elementImage = element.querySelector('img');
+  var elementPhoto = new Image();
+  element.replaceChild(elementPhoto, elementImage);
   container.appendChild(element);
-  var elementImage = new Image(182, 182);
-  /*elementImage.onload = function(evt) {
-  };*/
-  elementImage.onerror = function() {
+  elementPhoto.onload = function(evt) {
+    elementPhoto.setAttribute('src', evt.target.src);
+    elementPhoto.setAttribute('width', '182');
+    elementPhoto.setAttribute('height', '182');
+  };
+  elementPhoto.onerror = function() {
     element.classList.add('picture-load-failure');
   };
-  elementImage.src = data.url;
+  elementPhoto.src = data.url;
   return element;
 };
 
