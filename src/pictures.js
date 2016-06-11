6a9ce2b0008e0dc1730b6af44c8523d699932bc5
yaforelka <paravoznica@mail.ru>
2016-06-11 22:05:08 +0300

    В index удалены вспомогательные скрипты. В pictures.js добавлена функция отрисовки блоков с фото и функция для выполнения XMLHttp-запроса

--- src/pictures.js
+++ src/pictures.js
@@ -1,48 +1,89 @@
 'use strict';
-function queryJSONP(url, callback) {
+/*function queryJSONP(url, callback) {
   window.__picturesLoadCallback = function(data) {
     callback(data);
   };
   var script = document.createElement('script');
   script.setAttribute('src', url);
   document.body.appendChild(script);
-}
+}*/
 
-var filterContainer = document.querySelector('.filters');
-filterContainer.classList.add('hidden');
+(function() {
+  var filterContainer = document.querySelector('.filters');
+  filterContainer.classList.add('hidden');
 
-var pictureContainer = document.querySelector('.pictures');
-var templateElement = document.querySelector('template');
-var elementToClone;
+  var pictureContainer = document.querySelector('.pictures');
+  var templateElement = document.querySelector('template');
+  var elementToClone;
 
-if ('content' in templateElement) {
-  elementToClone = templateElement.content.querySelector('.picture');
-} else {
-  elementToClone = templateElement.querySelector('.picture');
-}
-var getPictureElement = function(data, container) {
-  var element = elementToClone.cloneNode(true);
-  element.querySelector('.picture-comments').textContent = data.comments;
-  element.querySelector('.picture-likes').textContent = data.likes;
-  var elementImage = element.querySelector('img');
-  var elementPhoto = new Image();
-  element.replaceChild(elementPhoto, elementImage);
-  container.appendChild(element);
-  elementPhoto.onload = function(evt) {
-    elementPhoto.setAttribute('src', evt.target.src);
-    elementPhoto.setAttribute('width', '182');
-    elementPhoto.setAttribute('height', '182');
+  if ('content' in templateElement) {
+    elementToClone = templateElement.content.querySelector('.picture');
+  } else {
+    elementToClone = templateElement.querySelector('.picture');
+  }
+  var getPictureElement = function(data, container) {
+    var element = elementToClone.cloneNode(true);
+    element.querySelector('.picture-comments').textContent = data.comments;
+    element.querySelector('.picture-likes').textContent = data.likes;
+
+    var elementImage = element.querySelector('img');
+    var elementPhoto = new Image();
+
+    element.replaceChild(elementPhoto, elementImage);
+    container.appendChild(element);
+
+    elementPhoto.onload = function(evt) {
+      elementPhoto.setAttribute('src', evt.target.src);
+      elementPhoto.setAttribute('width', '182');
+      elementPhoto.setAttribute('height', '182');
+    };
+
+    elementPhoto.onerror = function() {
+      element.classList.add('picture-load-failure');
+    };
+
+    elementPhoto.src = data.url;
+    return element;
   };
-  elementPhoto.onerror = function() {
-    element.classList.add('picture-load-failure');
+
+  var getPictures = function(callback) {
+    pictureContainer.classList.add('pictures-loading');
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function(evt) {
+      var loadedData = JSON.parse(evt.target.response);
+      callback(loadedData);
+    };
+
+    xhr.timeout = 5000;
+    xhr.ontimeout = function() {
+      pictureContainer.classList.add('pictures-failure');
+    };
+
+    xhr.onerror = function() {
+      pictureContainer.classList.add('pictures-failure');
+    };
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        pictureContainer.classList.remove('pictures-loading');
+      }
+    };
+
+    xhr.open('GET', '//o0.github.io/assets/json/pictures.json', true);
+    xhr.send();
   };
-  elementPhoto.src = data.url;
-  return element;
-};
-queryJSONP('//up.htmlacademy.ru/assets/js_intensive/jsonp/pictures.js', function(pictures) {
-  pictures.forEach(function(picture) {
-    getPictureElement(picture, pictureContainer);
+
+  var renderPictures = function(pictures) {
+    pictures.forEach(function(picture) {
+      getPictureElement(picture, pictureContainer);
+    });
+  };
+
+  getPictures(function(loadedPictures) {
+    var pictures = loadedPictures;
+    renderPictures(pictures);
   });
-});
 
-filterContainer.classList.remove('hidden');
+  filterContainer.classList.remove('hidden');
+})();
