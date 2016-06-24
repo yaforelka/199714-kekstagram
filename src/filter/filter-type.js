a0283ca83b49098ff86ade0a502b093ff922a306
yaforelka <paravoznica@mail.ru>
2016-06-24 14:56:36 +0300

    Убраны кавычки у ключей

--- src/filter/filter-type.js
+++ src/filter/filter-type.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  'ALL': 'filter-popular',
-  'DATE': 'filter-new',
-  'COMMENTS': 'filter-discussed'
+  ALL: 'filter-popular',
+  DATE: 'filter-new',
+  COMMENTS: 'filter-discussed'
 };
