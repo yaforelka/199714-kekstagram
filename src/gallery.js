282539ca1b7471e2842b351cbaf831714d12e4b1
yaforelka <paravoznica@mail.ru>
2016-07-02 17:44:14 +0300

    Подкорректирована привязка контекста в конструкторе
    Подкорректирован один метод для экспорта.

--- src/gallery.js
+++ src/gallery.js
@@ -3,7 +3,6 @@
 var utils = require('./utils');
 
 var Gallery = function() {
-
   this.element = document.querySelector('.gallery-overlay');
   this.closeElement = this.element.querySelector('.gallery-overlay-close');
   this.likes = this.element.querySelector('.likes-count');
@@ -11,27 +10,27 @@ var Gallery = function() {
   this.preview = this.element.querySelector('.gallery-overlay-image');
 
   this.galleryPictures = [];
+  this.picturesSrc = [];
   this.activePicture = 0;
 
   this.savePictures = this.savePictures.bind(this);
+  this.getHashContent = this.getHashContent.bind(this);
+
   this._onCloseClickHandler = this._onCloseClickHandler.bind(this);
   this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
-  this.setActivePicture = this.setActivePicture.bind(this);
   this._onPhotoClick = this._onPhotoClick.bind(this);
-  this.showGallery = this.showGallery.bind(this);
-  this.hideGallery = this.hideGallery.bind(this);
-  this._onHashChange = this._onHashChange.bind(this);
-  window.addEventListener('hashchange', this._onHashChange);
+
+  window.addEventListener('hashchange', this._onHashChange.bind(this));
 };
 
 Gallery.prototype.savePictures = function(pictures) {
   if (pictures !== this.galleryPictures) {
     this.galleryPictures = [];
+    this.picturesSrc = [];
     this.galleryPictures = pictures;
 
     pictures.forEach(function(pic) {
-      var pictureElement = new Image();
-      pictureElement.src = pic;
+      this.picturesSrc.push(pic.url);
     }, this);
   }
 };
@@ -48,15 +47,10 @@ Gallery.prototype._onDocumentKeyDown = function(evt) {
 };
 
 Gallery.prototype.setActivePicture = function(actPicture) {
-  this.galleryPictures.filter(function(item, i) {
-    if (item.url === actPicture) {
-      this.activePicture = i;
-      this.preview.src = actPicture;
-      this.likes.textContent = item.likes;
-      this.comments.textContent = item.comments;
-    }
-    return this.activePicture;
-  }, this);
+  this.activePicture = this.picturesSrc.indexOf(actPicture);
+  this.preview.src = this.galleryPictures[this.activePicture].url;
+  this.likes.textContent = this.galleryPictures[this.activePicture].likes;
+  this.comments.textContent = this.galleryPictures[this.activePicture].comments;
 };
 
 Gallery.prototype._onPhotoClick = function() {
@@ -64,8 +58,8 @@ Gallery.prototype._onPhotoClick = function() {
   if (this.activePicture === this.galleryPictures.length) {
     this.activePicture = 0;
   }
-  var currentSrc = this.galleryPictures[this.activePicture].url;
-  window.location.hash = '#photo/' + currentSrc;
+  var nextSrc = this.picturesSrc[this.activePicture];
+  window.location.hash = '#photo/' + nextSrc;
 };
 
 Gallery.prototype.showGallery = function(picture) {
@@ -84,25 +78,29 @@ Gallery.prototype.hideGallery = function() {
   this.closeElement.removeEventListener('click', this._onCloseClickHandler);
 };
 
+Gallery.prototype.getHashContent = function() {
+  if (location.hash !== '') {
+    this._onHashChange();
+  }
+};
+
 Gallery.prototype._onHashChange = function() {
   var hash = window.location.hash;
   var getPhotoRegExp = /#photo\/(\S+)/.exec(hash);
   if (getPhotoRegExp) {
-    if (this.galleryPictures.some(function(item) {
-      return item.url === getPhotoRegExp[1];
-    })) {
+    if (this.picturesSrc.indexOf(getPhotoRegExp[1]) > -1) {
       this.showGallery(getPhotoRegExp[1]);
     } else {
       this.hideGallery();
     }
   } else {
-    this.hideGallery();
+    location.hash = '';
   }
 };
 
 var photoGallery = new Gallery();
 
 module.exports = {
-  showGallery: photoGallery._onHashChange,
+  getHashContent: photoGallery.getHashContent,
   savePictures: photoGallery.savePictures
 };
